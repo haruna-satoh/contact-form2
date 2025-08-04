1f0b81609edbb06e6810a8102b66acd5e46d61d1
haruna <news.sigeak@gmail.com>
2025-08-05 06:20:00 +0900

    入力画面のレイアウト

---
+++ src/resources/views/index.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('css')
+    <link rel="stylesheet" href="{{ asset('css/index.css') }}">
+@endsection
+
+@section('content')
+    <h2>お問い合わせ</h2>
+    <form action="/contacts/confirm" method="post">
+        @csrf
+        <div>
+            <span>お名前</span>
+            <span>必須</span>
+            <input type="text" name="name" placeholder="テスト太郎" value="{{ old('name') }}">
+        </div>
+        <div>
+            <span>メールアドレス</span>
+            <span>必須</span>
+            <input type="text" name="email" placeholder="test@example.com" value="{{ old('email') }}">
+        </div>
+        <div>
+            <span>電話番号</span>
+            <span>必須</span>
+            <input type="text" name="tel" placeholder="09012345678" value="{{ old('tel') }}">
+        </div>
+        <div>
+            <span>お問い合わせ内容</span>
+            <textarea name="contact" cols="30" rows="10" placeholder="資料をいただきたいです"></textarea>
+        </div>
+        <button type="submit">送信</button>
+    </form>
+@endsection
+
+

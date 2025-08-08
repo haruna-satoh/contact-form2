5456071391e7de554e47aa4b06edf8e4e67b4ff8
haruna <news.sigeak@gmail.com>
2025-08-08 22:41:51 +0900

    要素のクラス分け

--- src/resources/views/confirm.blade.php
+++ src/resources/views/confirm.blade.php
@@ -1,36 +1,45 @@
 @extends('layouts.app')
 
-@section('content')
-    <h2>お問い合わせ内容確認</h2>
-<form action="/contacts" method="post">
-    @csrf
-    <table>
-        <tr>
-            <th>お名前</th>
-            <td>
-                <input type="text" name="name" value="{{ $contact['name'] }}" readonly >
-            </td>
-        </tr>
-        <tr>
-            <th>メールアドレス</th>
-            <td>
-                <input type="text" name="email" value="{{ $contact['email'] }}" readonly>
-            </td>
-        </tr>
-        <tr>
-            <th>電話番号</th>
-            <td>
-                <input type="text" name="tel" value="{{ $contact['tel'] }}" readonly>
-            </td>
-        </tr>
-        <tr>
-            <th>お問い合わせ内容</th>
-            <td>
-                <input type="text" name="content" value="{{ $contact['content'] }}" readonly>
-            </td>
-        </tr>
-    </table>
+@section('css')
+    <link rel="stylesheet" href="{{ asset('css/confirm.css') }}">
+@endsection
 
-    <button type="submit">送信</button>
-</form>
+@section('content')
+<div class="confirm__content">
+    <div class="confirm__title">
+        <h2>お問い合わせ内容確認</h2>
+    </div>
+    <form class="form" action="/contacts" method="post">
+        @csrf
+        <table class="confirm-table__inner">
+            <tr>
+                <th class="confirm-table__title">お名前</th>
+                <td class="confirm-table__item">
+                    <input type="text" name="name" value="{{ $contact['name'] }}" readonly >
+                </td>
+            </tr>
+            <tr>
+                <th class="confirm-table__title">メールアドレス</th>
+                <td class="confirm-table__item">
+                    <input type="text" name="email" value="{{ $contact['email'] }}" readonly>
+                </td>
+            </tr>
+            <tr>
+                <th class="confirm-table__title">電話番号</th>
+                <td class="confirm-table__item">
+                    <input type="text" name="tel" value="{{ $contact['tel'] }}" readonly>
+                </td>
+            </tr>
+            <tr>
+                <th class="confirm-table__title">お問い合わせ内容</th>
+                <td class="confirm-table__item">
+                    <input type="text" name="content" value="{{ $contact['content'] }}" readonly>
+                </td>
+            </tr>
+        </table>
+        <div class="confirm-button">
+            <button class="confirm-button__submit" type="submit">送信</button>
+        </div>
+    </form>
+</div>
 @endsection

50459e956dade91bc456cf45e930133d8578e78f
haruna <news.sigeak@gmail.com>
2025-08-08 22:43:40 +0900

    cssファイルの読み込みと要素のクラス分け

--- src/resources/views/thanks.blade.php
+++ src/resources/views/thanks.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.app')
 
+@section('css')
+    <link rel="stylesheet" href="{{ asset('css/thanks.css') }}">
+@endsection
+
 @section('content')
+<div class="thanks__content">
     <h2>お問い合わせありがとうございます</h2>
+</div>
 @endsection
